1aff7af442186094c0a5eeb65282aa71897d806c
TheGreatRay <46875858+RayZGit@users.noreply.github.com>
2021-01-23 09:59:53 -0500

    The basic functionalities for the storage

--- src/server/StoreDisk/IStoreDisk.java
+++ src/server/StoreDisk/IStoreDisk.java
@@ -2,6 +2,6 @@ package server.StoreDisk;
 
 public interface IStoreDisk {
     public void put(String key, String value);
-    public void get(String key);
+    public String get(String key);
     public boolean contain(String key);
 }
