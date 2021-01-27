976394c50009d833f4d1a7e39d53c14441b543e6
Shuoer <shuoer.wang@mail.utoronto.ca>
2021-01-27 05:53:28 -0500

    Update Performance.java

--- src/testing/Performance.java
+++ src/testing/Performance.java
@@ -3,21 +3,175 @@ package testing;
 import app_kvServer.KVServer;
 import client.KVStore;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 public class Performance {
     KVServer kvServer;
+    int total;
+    int type;
+    int port;
+    File file;
 
-    public Performance (int port, int cacheSize, String strategy) {
+    public Performance (int port, int cacheSize, String strategy, int total) {
+        try {
+            file = new File("performance.txt");
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        this.port = port;
         kvServer = new KVServer(port, cacheSize, strategy);
+        this.total = total;
         new Thread(kvServer).start();
     }
 
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void testLatency(){
+        KVStore client = new KVStore("localhost", port);
+        Random rand = new Random();
+        try {
+            client.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int flag = rand.nextInt(10);
+        int putkey = rand.nextInt(100);
+        int putVal = rand.nextInt(10000);
+        int getKey = rand.nextInt(100);
+
+        long start = System.nanoTime();
+        long highest = -1;
+
+        for (int i = 0; i < total; i++) {
+            try {
+                long temp1 = System.nanoTime();
+                if (type == 1) {
+                    if (flag < 5) client.put(Integer.toString(putkey), Integer.toString(putVal));
+                    else client.get(Integer.toString(getKey));
+                } else if (type == 2) {
+                    if (flag < 2) client.put(Integer.toString(putkey), Integer.toString(putVal));
+                    else client.get(Integer.toString(getKey));
+                } else {
+                    if (flag < 8) client.put(Integer.toString(putkey), Integer.toString(putVal));
+                    else client.get(Integer.toString(getKey));
+                }
+                long temp2 = System.nanoTime();
+                if (temp2 - temp1 > highest) highest = temp2 - temp1;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        long end = System.nanoTime();
+        long timePass = start - end;
+        double latency = (double) (timePass/1000000000) / total;
+        try {
+            FileWriter fw = new FileWriter("resources\\performance.txt", true);
+            fw.write("Highest latency of the operations is: " + highest / 1000000000);
+            fw.write("Average latency is: " + latency + " operations per second.");
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void testMultiClientThroughput(int numClient) {
-        KVStore kvClient = new KVStore("localhost", 50000);
+        Thread[] threads = new Thread[numClient];
+        for (int i = 0; i < numClient; i++) {
+            threads[i] =  new Thread(new Runnable() {
+                KVStore client;
+
+                @Override
+                public void run() {
+                    client = new KVStore("localhost", port);
+                    Random rand = new Random();
+                    try {
+                        client.connect();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    for(int j = 0; j < total; j++){
+                        try {
+                            int flag = rand.nextInt(10);
+                            int putkey = rand.nextInt(100);
+                            int putVal = rand.nextInt(10000);
+                            int getKey = rand.nextInt(100);
+                            if (type == 1) {
+                                if (flag < 5) client.put(Integer.toString(putkey), Integer.toString(putVal));
+                                else client.get(Integer.toString(getKey));
+                            } else if (type == 2) {
+                                if (flag < 2) client.put(Integer.toString(putkey), Integer.toString(putVal));
+                                else client.get(Integer.toString(getKey));
+                            } else {
+                                if (flag < 8) client.put(Integer.toString(putkey), Integer.toString(putVal));
+                                else client.get(Integer.toString(getKey));
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            });
+        }
+        long start = System.nanoTime();
+        for (int i = 0; i < numClient; i++) {
+            threads[i].start();
+        }
+        for (int i = 0; i < numClient; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        long end = System.nanoTime();
+        long timePass = start - end;
+        double throughput = total * numClient / (double) (timePass/1000000000);
+        try {
+            FileWriter fw = new FileWriter("resources\\performance.txt", true);
+            fw.write("Multi-client throughput is: " + throughput + " operations per second.");
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int cacheSize = 200;
-        int port = 50000;
+        int port = 50010;
+
         String cacheStrategy = "FIFO";
+        Performance performance = new Performance(port, cacheSize, cacheStrategy, 1000);
+        performance.setType(1);
+        System.out.println("Testing 50% put, 50% get: ");
+        //performance.testMultiClientThroughput(10);
+        performance.testLatency();
+        performance.setType(2);
+        System.out.println("Testing 20% put, 80% get: ");
+        //performance.testMultiClientThroughput(10);
+        //performance.testLatency();
+        performance.setType(3);
+        System.out.println("Testing 80% put, 20% get: ");
+        //performance.testMultiClientThroughput(10);
+        //performance.testLatency();
     }
 }
